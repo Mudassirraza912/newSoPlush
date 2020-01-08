15d8f803baca3250a460d6999e420a8c51dc1b68
Mudassirraza912 <razamudassir912@gmail.com>
2020-01-08 12:26:12 +0500

    182020

--- src/components/Authentication/userLogin.js
+++ src/components/Authentication/userLogin.js
@@ -401,6 +401,19 @@ const { width, height } = Dimensions.get("window")
 
 
 
+// GoogleSignin.configure({
+//     scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+//     webClientId: '240404485205-70t4nc6ok3jh03tu4dooro4nru85ec6i.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+//     offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+//     hostedDomain: '', // specifies a hosted domain restriction
+//     loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
+//     forceConsentPrompt: true, // [Android] if you want to show the authorization prompt at each login.
+//     accountName: '', // [Android] specifies an account name on the device that should be used
+//     iosClientId: '240404485205-70t4nc6ok3jh03tu4dooro4nru85ec6i.apps.googleusercontent.com', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+//   });
+
+  
+
 GoogleSignin.configure({
     scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
     webClientId: '240404485205-70t4nc6ok3jh03tu4dooro4nru85ec6i.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
@@ -411,10 +424,6 @@ GoogleSignin.configure({
     accountName: '', // [Android] specifies an account name on the device that should be used
     iosClientId: '240404485205-70t4nc6ok3jh03tu4dooro4nru85ec6i.apps.googleusercontent.com', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
   });
-
-  
-
-
 
 
 
